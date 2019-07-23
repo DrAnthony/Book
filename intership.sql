d6f4b78d76d8723a5f5d050eb2acad0e96112835
wangdi <w826581298@163.com>
2019-07-23 14:39:50 +0800

    登陆成功返回信息 ，msg字段添加cookies，更新intership.sql文件

--- intership.sql
+++ intership.sql
@@ -1,18 +1,13 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options,
-use mysqldump --help
--- MySQL dump 10.13  Distrib 5.7.26, for Win64 (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.38-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: intership
 -- ------------------------------------------------------
--- Server version	5.7.26-log
+-- Server version	10.1.38-MariaDB-0+deb9u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
 /*!40103 SET TIME_ZONE = '+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
@@ -47,7 +42,7 @@ CREATE TABLE `book`
     CONSTRAINT `p_fk` FOREIGN KEY (`press`) REFERENCES `press` (`id`),
     CONSTRAINT `t_fk` FOREIGN KEY (`type`) REFERENCES `booktype` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 12
+  AUTO_INCREMENT = 15
   DEFAULT CHARSET = utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -59,16 +54,19 @@ LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book`
     DISABLE KEYS */;
 INSERT INTO `book`
-VALUES (1, 3, '三体', '刘慈欣', 32, '1999-02-01', 10, 7, 0.00, 1, NULL, 1),
+VALUES (1, 3, '三体', '刘慈欣', 32, '1999-02-01', 11, 11, 25.00, 1, '9787229030933', 6),
+       (2, 1, '高等数学（第七版 上册）', '同济大学数学系', 6, '2019-07-01', 100, 100, 15.00, 1, '9787040396638', 0),
        (3, 3, '美国众神：十周年作者修订版', '尼尔·盖曼', 34, '2017-05-01', 100, 100, 69.90, 1, '9787550297142', 0),
-       (4, 34, '大学国际化的历程', '程星', 11, '2014-09-01', 100, 100, 40.20, 1, '9787100106252', 0),
+       (4, 34, '大学国际化的历程', '程星', 11, '2014-09-01', 1001, 1001, 40.20, 1, '9787100106252', 9),
        (5, 8, '居里夫人传', '艾芙·居里', 11, '1984-07-01', 20, 20, 28.40, 1, '9787100001076', 0),
-       (6, 8, '爱因斯传', '亚伯拉罕·派斯', 11, '2004-09-01', 20, 20, 69.00, 1, '9787100038829', 0),
+       (6, 8, '爱因斯传', '亚伯拉罕·派斯', 11, '2004-09-01', 20, 20, 69.00, 1, '9787100038829', 1),
        (7, 10, '逻辑思维训练500题', '于雷', 15, '2013-01-01', 20, 20, 39.00, 1, '9787302303619', 0),
-       (8, 27, 'C语言零基础入门学习：带你学C带你飞', '小甲鱼', 15, '2019-05-01', 20, 20, 79.00, 0, '9787302505945', 0),
-       (9, 10, '中国哲学简史', '冯友兰', 20, '2013-01-01', 20, 20, 38.00, 0, '9787301215692', 0),
-       (10, 15, '博弈论与经济行为', '冯·诺伊曼,摩根斯坦', 20, '2018-04-01', 20, 20, 158.00, 0, '9787301293263', 0),
-       (11, 9, '中华经典名著全本全注全译丛书9：老子', '汤漳平，王朝华', 13, '2014-07-01', 20, 20, 23.00, 1, '9787101100747', 0);
+       (8, 27, 'C语言零基础入门学习：带你学C带你飞', '小甲鱼', 15, '2019-07-21', 20, 20, 79.00, 1, '9787302505945', 1),
+       (9, 10, '中国哲学简史', '冯友兰', 20, '2019-07-22', 20, 20, 38.00, 1, '9787301215692', 0),
+       (10, 15, '博弈论与经济行为', '冯·诺伊曼,摩根斯坦', 20, '2019-07-22', 20, 20, 158.00, 1, '9787301293263', 0),
+       (11, 9, '中华经典名著全本全注全译丛书9：老子', '汤漳平，王朝华', 13, '2014-07-01', 20, 20, 23.00, 1, '9787101100747', 0),
+       (12, 27, '阿里巴巴Java开发手册', '杨冠宝', 10, '2018-01-01', 20, 20, 35.00, 1, '9787121332319', 2),
+       (13, 6, '中国动画经典升级版:没头脑和不高兴 [3-6岁]', '任溶溶，上海美术电影制片厂有限公司', 7, '2019-07-22', 20, 20, 14.80, 1, '9787513593625', 0);
 /*!40000 ALTER TABLE `book`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -153,8 +151,7 @@ CREATE TABLE `captcha`
     KEY `su_fk` (`s_id`),
     CONSTRAINT `su_fk` FOREIGN KEY (`s_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 3
-  DEFAULT CHARSET = latin1;
+  DEFAULT CHARSET = utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,8 +161,6 @@ CREATE TABLE `captcha`
 LOCK TABLES `captcha` WRITE;
 /*!40000 ALTER TABLE `captcha`
     DISABLE KEYS */;
-INSERT INTO `captcha`
-VALUES (2, 2, 'x81Xag8dj9ZpFgR22md3zw==', '2019-07-23');
 /*!40000 ALTER TABLE `captcha`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -185,7 +180,7 @@ CREATE TABLE `code`
     PRIMARY KEY (`id`),
     UNIQUE KEY `phone` (`phone`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 15
+  AUTO_INCREMENT = 23
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -197,7 +192,12 @@ LOCK TABLES `code` WRITE;
 /*!40000 ALTER TABLE `code`
     DISABLE KEYS */;
 INSERT INTO `code`
-VALUES (1, '13611111111', '123456');
+VALUES (1, '13611111111', '123456'),
+       (17, '17860638986', '446724'),
+       (18, '13667155495', '037054'),
+       (20, '13622222222', '123456'),
+       (21, '13271356979', '966485'),
+       (22, '15328411322', '707707');
 /*!40000 ALTER TABLE `code`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -212,13 +212,13 @@ DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments`
 (
     `id`      int(11)    NOT NULL AUTO_INCREMENT,
-    `s_id`    int(11) DEFAULT NULL,
+    `s_id`    int(11)    NOT NULL,
     `content` mediumtext NOT NULL,
     PRIMARY KEY (`id`),
     KEY `u_fk` (`s_id`),
-    CONSTRAINT `u_fk` FOREIGN KEY (`s_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+    CONSTRAINT `u_fk` FOREIGN KEY (`s_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 5
+  AUTO_INCREMENT = 7
   DEFAULT CHARSET = utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -230,8 +230,8 @@ LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments`
     DISABLE KEYS */;
 INSERT INTO `comments`
-VALUES (3, NULL, 'This is a test comment'),
-       (4, NULL, 'This is a test comment');
+VALUES (4, 4, '页面刷新、获取头像没有添加api、注册页面按钮溢出'),
+       (6, 9, 'kkkkkkkkkkkkk');
 /*!40000 ALTER TABLE `comments`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -306,8 +306,8 @@ DROP TABLE IF EXISTS `stubook`;
 CREATE TABLE `stubook`
 (
     `id`         int(11)    NOT NULL AUTO_INCREMENT,
-    `s_id`       int(11)             DEFAULT NULL,
-    `b_id`       int(11)             DEFAULT NULL,
+    `s_id`       int(11)    NOT NULL,
+    `b_id`       int(11)    NOT NULL,
     `b_date`     date       NOT NULL,
     `r_date`     date       NOT NULL,
     `overtime`   int(11)    NOT NULL DEFAULT '0',
@@ -315,10 +315,10 @@ CREATE TABLE `stubook`
     PRIMARY KEY (`id`),
     KEY `s_fk` (`s_id`),
     KEY `b_fk` (`b_id`),
-    CONSTRAINT `b_fk` FOREIGN KEY (`b_id`) REFERENCES `book` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-    CONSTRAINT `s_fk` FOREIGN KEY (`s_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+    CONSTRAINT `b_fk` FOREIGN KEY (`b_id`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `s_fk` FOREIGN KEY (`s_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 7
+  AUTO_INCREMENT = 23
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -330,12 +330,24 @@ LOCK TABLES `stubook` WRITE;
 /*!40000 ALTER TABLE `stubook`
     DISABLE KEYS */;
 INSERT INTO `stubook`
-VALUES (1, NULL, 1, '2019-07-14', '2019-09-14', 0, 1),
-       (2, NULL, 1, '2019-07-14', '2019-07-24', 0, 1),
-       (3, NULL, 1, '2019-07-17', '2019-09-17', 0, 0),
-       (4, 2, 1, '2019-07-19', '2019-09-19', 0, 0),
-       (5, 2, 1, '2019-07-19', '2019-09-19', 0, 0),
-       (6, 2, 1, '2019-07-19', '2019-09-19', 0, 0);
+VALUES (3, 3, 1, '2019-07-17', '2019-09-17', 0, 0),
+       (4, 4, 1, '2019-07-21', '2019-09-21', 0, 1),
+       (5, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (6, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (7, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (8, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (9, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (10, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (11, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (12, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (13, 2, 4, '2019-07-22', '2019-09-22', 0, 1),
+       (14, 2, 8, '2019-07-22', '2019-09-22', 0, 1),
+       (15, 2, 1, '2019-07-22', '2019-09-22', 0, 1),
+       (16, 2, 1, '2019-07-22', '2019-09-22', 0, 1),
+       (19, 7, 1, '2019-07-22', '2019-09-22', 0, 1),
+       (20, 2, 1, '2019-07-22', '2019-09-22', 0, 1),
+       (21, 9, 12, '2019-07-22', '2019-09-22', 0, 1),
+       (22, 9, 12, '2019-07-22', '2019-09-22', 0, 1);
 /*!40000 ALTER TABLE `stubook`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -361,10 +373,10 @@ CREATE TABLE `user`
     `photo`      varchar(20)          DEFAULT NULL,
     `nickname`   varchar(20)          DEFAULT NULL,
     `signature`  text,
-    `birthday`   date                 DEFAULT NULL,
+    `birthday`   date        NOT NULL DEFAULT '1949-10-01',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 6
+  AUTO_INCREMENT = 10
   DEFAULT CHARSET = utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -377,10 +389,21 @@ LOCK TABLES `user` WRITE;
     DISABLE KEYS */;
 INSERT INTO `user`
 VALUES (1, 'tom', 1, '4QrcOUm6Wau+VuBX8g+IPg==', '15863755633', 1, 10, 10, '2019-07-14 11:50:21', NULL, 'tom', NULL,
-        NULL),
-       (2, 'root', 0, '4QrcOUm6Wau+VuBX8g+IPg==', '13612345678', 1, 10, 7, '2019-07-23 13:23:08', '1563530021608.jpg',
+        '1949-10-01'),
+       (2, 'root', 0, '4QrcOUm6Wau+VuBX8g+IPg==', '13612345678', 1, 10, 10, '2019-07-22 21:09:15', '1563534637050.jpg',
         'testAdmin', 'for test', '1949-10-01'),
-       (5, NULL, 1, '123456', '13611111111', 2, 10, 10, '2019-07-22 09:35:53', NULL, NULL, NULL, NULL);
+       (3, 'tom', 1, '4QrcOUm6Wau+VuBX8g+IPg==', '13611111111', 1, 10, 8, '2019-07-22 10:42:04', '1563171230624.jpg',
+        'tom', 'I am an ordinary user.', '1999-02-14'),
+       (4, '王大爷', 1, 'lueSGJZetyySpUndWjMBEg==', '13667155495', 2, 10, 10, '2019-07-22 12:44:12', '1563756399760.png',
+        '王大爷', '这个团队也太牛皮了吧', '1999-02-14'),
+       (5, NULL, 1, 'qgcwXRW49XJ2BUulMJTFig==', '17860638986', 2, 10, 10, '2019-07-21 23:32:46', NULL, NULL, NULL,
+        '1949-10-01'),
+       (7, NULL, 1, '4QrcOUm6Wau+VuBX8g+IPg==', '13622222222', 2, 10, 10, '2019-07-22 22:10:50', NULL, NULL, NULL,
+        '1949-10-01'),
+       (8, NULL, 1, '4QrcOUm6Wau+VuBX8g+IPg==', '13271356979', 2, 10, 10, '2019-07-23 10:10:25', NULL, NULL, NULL,
+        '2019-07-22'),
+       (9, 'kk', 1, '4QrcOUm6Wau+VuBX8g+IPg==', '15328411322', 2, 10, 10, '2019-07-22 20:58:04', '1563800167315.jpg',
+        'kk', NULL, '2019-07-22');
 /*!40000 ALTER TABLE `user`
     ENABLE KEYS */;
 UNLOCK TABLES;
@@ -394,4 +417,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-23 14:12:30
+-- Dump completed on 2019-07-23 14:31:26
